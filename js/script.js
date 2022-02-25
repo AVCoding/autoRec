dc7a985c607dcaa276947201935b62a9c4d9b6e5
AVCoding <70842251+AVCoding@users.noreply.github.com>
2022-02-25 18:09:25 +0300

    wedwed

--- js/script.js
+++ js/script.js
@@ -12,6 +12,7 @@
   var  myID;
   //peer.on("open", function (id) {
     //document.getElementById("uuid").textContent = id;
+    document.getElementById("uuid").textContent = peer.id;
      console.log(peer);
     myID =  id;
   //});
