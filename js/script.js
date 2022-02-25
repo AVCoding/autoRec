c3d3a72bcdabd47529bd286d47f4f967d2870409
AVCoding <70842251+AVCoding@users.noreply.github.com>
2022-02-25 17:20:18 +0300

    ergerg

--- js/script.js
+++ js/script.js
@@ -96,7 +96,7 @@
       if (data == conn.peer ) {
         alert('ekav');
         videoRecOff(conn.peer);
-        conn.close();
+        conn.disconnect;
       }
     });
     conn.on('close', function(el){
