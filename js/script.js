504296bb51a7ab090b9a49fd6681673826688e52
AVCoding <70842251+AVCoding@users.noreply.github.com>
2022-02-25 18:13:28 +0300

    erferf

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@
   var  myID;
   //peer.on("open", function (id) {
     //document.getElementById("uuid").textContent = id;
-    document.getElementById("uuid").textContent = peer.id.toString();
+    document.getElementById("uuid").textContent = peer._id;
      console.log(peer);
     myID =  id;
   //});
